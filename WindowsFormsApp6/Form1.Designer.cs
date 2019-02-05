5b4ae00a9d52c19c07e2b22871e803df22953597
Mirilkin R. Nasirzada <MirilkinRn@code.local>
2019-02-05 14:30:26 +0400

    ilkin log in

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbx_login = new System.Windows.Forms.TextBox();
+            this.tbx_password = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbx_login
+            // 
+            this.tbx_login.Location = new System.Drawing.Point(31, 39);
+            this.tbx_login.Name = "tbx_login";
+            this.tbx_login.Size = new System.Drawing.Size(100, 20);
+            this.tbx_login.TabIndex = 0;
+            // 
+            // tbx_password
+            // 
+            this.tbx_password.Location = new System.Drawing.Point(31, 65);
+            this.tbx_password.Name = "tbx_password";
+            this.tbx_password.PasswordChar = '*';
+            this.tbx_password.Size = new System.Drawing.Size(100, 20);
+            this.tbx_password.TabIndex = 1;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(31, 91);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(100, 23);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 412);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.tbx_password);
+            this.Controls.Add(this.tbx_login);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbx_login;
+        private System.Windows.Forms.TextBox tbx_password;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        LOgEntities db = new LOgEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            var username = tbx_login.Text;
+            var password = tbx_password.Text;
+            if (db.Users.Any(item => item.Username == username && item.Userpassword == password))
+            {
+                MessageBox.Show("Login O" +
+                    "ldunuz Tebrikler");
+            }
+            else
+            {
+                MessageBox.Show("Birdaha yeniden yoxlayin");
+            }
+        }
+    }
+}
+
+
